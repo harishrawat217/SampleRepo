1aa9480fb3ceeef03c351e12bbcc10b717ad562f
harishrawat217 <43847594+harishrawat217@users.noreply.github.com>
2018-10-05 15:39:58 +0530

    Calculator UnitTest

---
+++ Calculator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Calculations
+{
+    public class Calculator
+
+    {
+        private Memory memory = new Memory();
+
+        public double Add(double x, double y)
+        {
+            var result = x + y;
+            memory.Store(x, "+", y, result);
+            return result;
+
+        }
+
+        public double Divide(double x, double y)
+        {
+            if (y == 0)
+                throw new ArgumentException("MESSAGE_INFINITY");
+            var result = x / y;
+            memory.Store(x, "/", y, result);
+
+            return result;
+        }
+
+
+        public string Replay()
+        {
+            return memory.ToString();
+        }
+
+        public string ReplayLast(int howMany = 1)
+        {
+            return memory.ToStringReverse(howMany);
+        }
+    }
+
+    internal class Memory
+    {
+        private IList<string> memory = new List<string>();
+
+        internal void Store(double x, string op, double y, double result)
+        {
+            memory.Add($"{x} {op} {y} = {result}");
+        }
+
+        private string Stringify(IList<string> list)
+        {
+            if (list.Count == 0)
+                return "Nothing To Replay";
+
+            return string.Join("; ", list);
+
+        }
+        internal String ToString()
+        {
+            return Stringify(memory);
+        }
+
+        internal String ToStringReverse(int howMany)
+        {
+            return Stringify(memory.Reverse().Take(howMany).ToList());
+
+        }
+
+    }
+}
---
+++ CalculatorSpecs.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculations;
+using NUnit.Framework;
+using Specification = NUnit.Framework.TestAttribute;
+
+namespace CalculatorSpecs
+{
+    public class CalculatorSpecs
+
+    {
+        private Calculator calculator;
+
+        [SetUp]
+        public void CreateCalculator()
+        {
+            calculator = new Calculator();
+        }
+        [Specification]
+        public void AddsTwoNumbers()
+        {
+            //Given
+      
+            //When
+            //Then
+
+            Assert.That(calculator.Add(2.0, 3.0), Is.EqualTo(5.0));
+            Assert.That(calculator.Add(3.0, 3.0), Is.EqualTo(6.0));
+        }
+
+        [Specification]
+        public void DividesTwoNumbersWith6DigitsPrecision()
+        {
+            //Given
+          
+
+            //When
+            //Then
+
+            Assert.That(calculator.Divide(2.0, 3.0), Is.EqualTo(0.6666666666).Within(0.0000001));
+
+        }
+
+        [Specification]
+        public void DivisionByZeroResultsInInifinity() 
+        {
+            try
+            {
+                calculator.Divide(2.0, 0);
+                Assert.Fail("Should Not Divide by Zero");
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual("MESSAGE_INFINITY", e.Message);
+            }
+        }
+
+        [Specification]
+        public void ReplaysAddOperation()
+        {
+            //given
+            calculator.Add(2, 3);
+            //When
+            string result = calculator.Replay();
+            Assert.That(result, Is.EqualTo("2 + 3 = 5"));
+        }
+        [Specification]
+        public void ReplaysDivision()
+        {
+            //given
+            calculator.Divide(6, 3);
+            //When
+            string result = calculator.Replay();
+            Assert.That(result, Is.EqualTo("6 / 3 = 2"));
+        }
+
+        [Specification]
+        public void ReplaysAllOperations()
+        {
+            //given
+            calculator.Add(2, 3);
+            calculator.Divide(6, 3);
+
+            //When
+            string result = calculator.Replay();
+            Assert.That(result, Is.EqualTo("2 + 3 = 5; 6 / 3 = 2"));
+
+        }
+
+        [Specification]
+        public void ReplaysNothingWhenNoOperationsArePerformed()
+        {
+            string result = calculator.Replay();
+            Assert.That(result, Is.EqualTo("Nothing To Replay"));
+
+        }
+
+        [Specification]
+        public void ReplaysLastOperation()
+        {
+            //given
+            calculator.Divide(6, 3);
+            //When
+            string result = calculator.ReplayLast();
+
+            Assert.That(result, Is.EqualTo("6 / 3 = 2"));
+
+        }
+
+        [Specification]
+        public void ReplaysLast2Operations()
+        {
+            //given
+            calculator.Add(2, 3);
+            calculator.Divide(6, 3);
+
+            //When
+            string result = calculator.ReplayLast(2);
+            Assert.That(result, Is.EqualTo("6 / 3 = 2; 2 + 3 = 5"));
+        }
+
+        [Specification]
+        public void DoesNotReplayInReverseWhenNoOperationIsPerformed()
+        {
+            string result = calculator.ReplayLast();
+            Assert.That(result, Is.EqualTo("Nothing To Replay"));
+
+        }
+
+        [Specification]
+        public void ReplaysAllInReverseWhenAskedForMoreOperationsThanPerformed()
+        {
+            //given
+            calculator.Add(2, 3);
+            calculator.Divide(6, 3);
+
+            //When
+            string result = calculator.ReplayLast(12);
+            Assert.That(result, Is.EqualTo("6 / 3 = 2; 2 + 3 = 5"));
+
+        }
+    }
+}
